d06a2891a238fb914c1dd7af56adbc7311cccc86
Merrin W. Dream <merrin@merrinworld.uk>
2026-09-04 16:52:26 +0100

    Preserve UK 70-scope subtotal under Ireland extension

--- tests/test_assessment_diagnosis_uk_v1.py
+++ tests/test_assessment_diagnosis_uk_v1.py
@@ -63,8 +63,9 @@ class AssessmentDiagnosisUKV1Tests(unittest.TestCase):
         self.assertEqual(41, len(base_routes))
         self.assertEqual(29, len(extension_routes))
         self.assertEqual(set(), set(base_routes) & set(extension_routes))
-        self.assertEqual(70, discovery.EXPECTED_SCOPED_ROUTE_COUNT)
-        self.assertEqual(70, len(discovery.POLICY["scope_provenance"]["routes"]))
+        self.assertEqual(70, len(base_routes) + len(extension_routes))
+        self.assertEqual(82, discovery.EXPECTED_SCOPED_ROUTE_COUNT)
+        self.assertEqual(82, len(discovery.POLICY["scope_provenance"]["routes"]))
         self.assertEqual(set(), set(self.base["intent_phrases"]) & set(self.extension["intent_phrases"]))
         discovery.validate_policy(index=self.index)
 
